906216a968b44c05aa615879f3ad12adab078996
jztzh <zhaohang@54315.com>
2017-12-16 13:59:38 +0800

    重构之前提交

--- lists/tests.py
+++ lists/tests.py
@@ -25,3 +25,15 @@ class HomePageTest(TestCase):
 
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)#decode() byte to Unicode
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		response = home_page(request)
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+			'home.html', 
+			{'new_item_text':'A new list item'}
+		)
+		self.assertEqual(response.content.decode(), expected_html)
